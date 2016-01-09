448ba24bb884ca1899e6c21354b6a540beddb8c1
Pavan Gudimetta <pgudimetta@gmail.com>
2016-01-08 19:06:17 -0500

    updated autocomplete.sh

--- Linux/autocomplete.sh
+++ Linux/autocomplete.sh
@@ -2,14 +2,13 @@
 #enters repo
 cd ~/.vim/bundle/YouCompleteMe
 
-#clone repo into vim
-git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle
-
 #update YouCompleteMe Repo
 git submodule update --init --recursive
 
 #install with all language features
-#./install.py --clang-completer --omnisharp-completer --gocode-completer --tern-completer
-
+if [ "$1" == "-a" ]
+then
+	./install.py --clang-completer --omnisharp-completer --gocode-completer --tern-completer
+fi
 # no C# for me lol xD
 ./install.py --clang-completer --gocode-completer --tern-completer
